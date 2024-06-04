e7b9e80d87ca1fe823aa4390ce94a53b66d33443
frogro <frankiin77@hotmail.com>
2024-06-04 21:29:45 +0200

    Update rotary.sh

--- rotary.sh
+++ rotary.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 evsieve --input /dev/input/event2 grab \
-        --hook key:t exec-shell="curl [Photobooth IP]:14711/commands/start-picture" \
-        --hook key:c exec-shell="curl [Photobooth IP]:14711/commands/start-collage" \
-        --hook key:p exec-shell="curl [Photobooth IP]:14711/commands/start-print" \
-        --hook key:v exec-shell="curl [Photobooth IP]:14711/commands/start-video" \
-        --hook key:u exec-shell="curl [Photobooth IP]:14711/commands/start-custom" \
-        --hook key:s exec-shell="curl [Photobooth IP]:14711/commands/shutdown-now" \
-        --hook key:enter exec-shell="curl [Photobooth IP]:14711/commands/rotary-btn \
-        --hook key:left exec-shell="curl [Photobooth IP]:14711/commands/rotary-ccw" \
-        --hook key:right exec-shell="curl [Photobooth IP]:14711/commands/rotary-cw"
-
-
+        --hook key:t exec-shell="curl 127.0.0.1:14711/commands/start-picture" \
+        --hook key:c exec-shell="curl 127.0.0.1:14711/commands/start-collage" \
+        --hook key:p exec-shell="curl 127.0.0.1:14711/commands/start-print" \
+        --hook key:v exec-shell="curl 127.0.0.1:14711/commands/start-video" \
+        --hook key:u exec-shell="curl 127.0.0.1:14711/commands/start-custom" \
+        --hook key:s exec-shell="curl 127.0.0.1:14711/commands/shutdown-now" \
+        --hook key:enter exec-shell="curl 127.0.0.1:14711/commands/rotary-btn-press" \
+        --hook key:left exec-shell="curl 127.0.0.1:14711/commands/rotary-ccw" \
+        --hook key:right exec-shell="curl 127.0.0.1:14711/commands/rotary-cw"
